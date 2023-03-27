6377eaf8644cd605075f64c35ac66ecd098c4466
cass <76407605+cleclair71@users.noreply.github.com>
2023-03-27 14:24:51 -0400

    readme link

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,9 +42,9 @@ router.get('/:id', async (req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      "product_name": "Basketball",
-      "price": 200.00,
-      "stock": 3,
+      "product_name": "Hat",
+      "price": 50.00,
+      "stock": 5,
       "tagIds": [1, 2, 3, 4]
     }
   */
